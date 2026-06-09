9370e9db5f26d5b2e47f1ce266d2b9ee057767ed
drlizlondon <drlizlondon@users.noreply.github.com>
2026-06-09 12:31:48 +0000

    deploy: fffc14f95888bc4de53a436ed0ca3ff9da7302d0

---
+++ founder.js
@@ -0,0 +1,13 @@
+// Founding Access payment link.
+// Paste your Stripe Payment Link URL between the quotes below.
+// Leave it empty to show the "opening shortly" message instead.
+window.BPP_FOUNDING_ACCESS_URL = '';
+
+window.openFoundingAccess = function () {
+  var url = (window.BPP_FOUNDING_ACCESS_URL || '').trim();
+  if (url) {
+    window.location.href = url;
+  } else {
+    alert('Founder payments are opening shortly. Join the waitlist and we will send your invite.');
+  }
+};
